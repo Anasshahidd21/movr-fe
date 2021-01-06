6fa55188245b58e63ccca15e05ae74b568ff170b
sanihaseeb <sanihaseeb@hotmail.com>
2021-01-06 19:09:38 +0500

    Current location marker

--- movr-ui/src/components/Map/MapComponent.jsx
+++ movr-ui/src/components/Map/MapComponent.jsx
@@ -1,8 +1,32 @@
 import React, { Component } from "react";
-import { Map, GoogleApiWrapper } from "google-maps-react";
+import { Map, GoogleApiWrapper, InfoWindow, Marker } from "google-maps-react";
 import mapStyles from "./MapStyles";
+import CurrentLocation from './Map';
 
 export class MapContainer extends Component {
+
+    state = {
+        showingInfoWindow: false,  // Hides or shows the InfoWindow
+        activeMarker: {},          // Shows the active marker upon click
+        selectedPlace: {}          // Shows the InfoWindow to the selected place upon a marker
+      };
+
+      onMarkerClick = (props, marker, e) =>
+    this.setState({
+      selectedPlace: props,
+      activeMarker: marker,
+      showingInfoWindow: true
+    });
+
+  onClose = props => {
+    if (this.state.showingInfoWindow) {
+      this.setState({
+        showingInfoWindow: false,
+        activeMarker: null
+      });
+    }
+  };
+
   render() {
     return (
       <Map
@@ -13,11 +37,25 @@ export class MapContainer extends Component {
           lat: 45.4212,
           lng: -75.6972,
         }}
+        >
+        <Marker
+        onClick={this.onMarkerClick}
+        name={'Kenyatta International Convention Centre'}
       />
+      <InfoWindow
+        marker={this.state.activeMarker}
+        visible={this.state.showingInfoWindow}
+        onClose={this.onClose}
+      >
+        <div>
+          <h4>{this.state.selectedPlace.name}</h4>
+        </div>
+      </InfoWindow>
+      </Map>
     );
   }
 }
 
 export default GoogleApiWrapper({
-  apiKey: process.env.REACT_APP_API_KEY,
+  apiKey: process.env.REACT_APP_API_KEY || 'AIzaSyAVTfdqZG0395y3iJ_8dcoZPjGshf-9K64',
 })(MapContainer);
