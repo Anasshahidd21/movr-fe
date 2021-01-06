81d62744f4835ae652d8f0c43eff8d45b7776d0e
Anas Shahid <muhammad.shahid@ericsson.com>
2021-01-06 18:34:30 +0500

    cleaned repo

--- movr-ui/src/components/Map/MapComponent.jsx
+++ movr-ui/src/components/Map/MapComponent.jsx
@@ -1,10 +1,6 @@
-import React, { Component } from 'react';
-import { Map, GoogleApiWrapper } from 'google-maps-react';
-
-const mapStyles = {
-  width: '100%',
-  height: '100%'
-};
+import React, { Component } from "react";
+import { Map, GoogleApiWrapper } from "google-maps-react";
+import mapStyles from "./MapStyles";
 
 export class MapContainer extends Component {
   render() {
@@ -13,17 +9,15 @@ export class MapContainer extends Component {
         google={this.props.google}
         zoom={16}
         style={mapStyles}
-        initialCenter={
-          {
-            lat: 45.4212,
-            lng: -75.6972
-          }
-        }
+        initialCenter={{
+          lat: 45.4212,
+          lng: -75.6972,
+        }}
       />
     );
   }
 }
 
 export default GoogleApiWrapper({
-  apiKey: 'AIzaSyAVTfdqZG0395y3iJ_8dcoZPjGshf-9K64'
+  apiKey: process.env.REACT_APP_API_KEY,
 })(MapContainer);
