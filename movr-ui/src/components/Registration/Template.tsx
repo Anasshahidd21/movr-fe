253985129535bd22082ddf20fc960b1e2b343207
Anas Shahid <muhammad.shahid@ericsson.com>
2021-06-07 20:22:24 -0400

    updated css for margin

--- movr-ui/src/components/Registration/Template.tsx
+++ movr-ui/src/components/Registration/Template.tsx
@@ -1,10 +1,8 @@
 import { MDBInput, MDBBtn, MDBCard, MDBCardBody } from "mdbreact";
 import "./TemplateRegistration.css";
 import React, { useState } from "react";
-import fire from "../../firebase" 
-require('firebase/auth');
-
-
+import fire from "../../firebase";
+require("firebase/auth");
 
 interface IformData {
   label: string;
@@ -19,13 +17,12 @@ interface inputProps {
 }
 
 const Template = ({ formData, title, message }: inputProps) => {
-
   const [errorMessage, setErrorMessage] = useState("");
   const [password, setPassword] = useState("");
   const [email, setEmail] = useState("");
   const user = fire.auth().currentUser;
 
-  const onChange = React.useCallback(e => {
+  const onChange = React.useCallback((e) => {
     switch (e.target.name) {
       case "password":
         setErrorMessage("");
@@ -39,39 +36,37 @@ const Template = ({ formData, title, message }: inputProps) => {
     }
   }, []);
 
-
   return (
-
-  <div className='registration-component'>
-    <div className="registrationAlignment">
-      <MDBCard>
-        <MDBCardBody>
-          <form>
-            <p className="h4 text-center py-4">{title}</p>
-            <div className="grey-text">
-              {formData.map((data: any, i: any) => {
-                return (
-                  <MDBInput
-                    key={i}
-                    label={data.label}
-                    group
-                    type={data.inputType}
-                    placeholder={data.inputPlaceHolder}
-                    onChange={onChange}
-                  />
-                );
-              })}
-            </div>
-            <div className="text-center py-4 mt-3">
-              <p className="message">{message}</p>
-              <MDBBtn color="cyan" type="submit">
-                {title}
-              </MDBBtn>
-            </div>
-          </form>
-        </MDBCardBody>
-      </MDBCard>
-    </div>
+    <div className="registration-component">
+      <div className="registrationAlignment">
+        <MDBCard className="top">
+          <MDBCardBody>
+            <form>
+              <p className="h4 text-center py-4">{title}</p>
+              <div className="grey-text">
+                {formData.map((data: any, i: any) => {
+                  return (
+                    <MDBInput
+                      key={i}
+                      label={data.label}
+                      group
+                      type={data.inputType}
+                      placeholder={data.inputPlaceHolder}
+                      onChange={onChange}
+                    />
+                  );
+                })}
+              </div>
+              <div className="text-center py-4 mt-3">
+                <p className="message">{message}</p>
+                <MDBBtn color="cyan" type="submit">
+                  {title}
+                </MDBBtn>
+              </div>
+            </form>
+          </MDBCardBody>
+        </MDBCard>
+      </div>
     </div>
   );
 };
