77bc9733081dfa929a80ba88b03c8283e07f2481
Anas Shahid <muhammad.shahid@ericsson.com>
2021-06-10 16:53:46 -0400

    encapsulated utility methods

--- movr-ui/src/components/Registration/RegService.ts
+++ movr-ui/src/components/Registration/RegService.ts
@@ -2,28 +2,6 @@ import fire from "../../firebase";
 import RegistrationEmitters from "./RegistrationEmitters";
 import { userData } from "./RegistrationInterfaces";
 
-export async function signUpUser({
-  email,
-  password,
-}: userData): Promise<string | undefined> {
-  try {
-    await fire.auth().createUserWithEmailAndPassword(email, password);
-  } catch (e) {
-    return e.message;
-  }
-}
-
-export async function signInUser({
-  email,
-  password,
-}: userData): Promise<string | undefined> {
-  try {
-    await fire.auth().signInWithEmailAndPassword(email, password);
-  } catch (e) {
-    return e.message;
-  }
-}
-
 export class regService extends RegistrationEmitters {
   protected emitter: RegistrationEmitters | undefined;
 
@@ -34,11 +12,11 @@ export class regService extends RegistrationEmitters {
 
   protected init = () => {
     this.onDidSignUpEmitter?.on(this.SIGNUP, async ({ email, password }) => {
-      await signUpUser({ email, password });
+      await this.signUpUser({ email, password });
     });
 
     this.onDidSignInEmitter.on(this.SIGNIN, async ({ email, password }) => {
-      await signInUser({ email, password });
+      await this.signInUser({ email, password });
     });
   };
 
