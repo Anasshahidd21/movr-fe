d21ef9549dda03fd5a1872733ed9c73f47e98062
Anas Shahid <muhammad.shahid@ericsson.com>
2021-06-10 10:05:08 -0400

    fixed merging errors

--- movr-ui/src/components/Registration/RegService.ts
+++ movr-ui/src/components/Registration/RegService.ts
@@ -1,17 +1,7 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 392a674... event emitters added
 import { globalEvent } from "@billjs/event-emitter";
 import { EventEmitter } from "events";
 import fire from "../../firebase";
 import RegistrationEmitters from "./RegistrationEmitters";
-<<<<<<< HEAD
-=======
-import fire from "../../firebase";
->>>>>>> 149a9bc... added the functionality to signup more abstractly
-=======
->>>>>>> 392a674... event emitters added
 import { userData } from "./Template";
 
 export async function signUpUser({
@@ -36,19 +26,10 @@ export async function signInUser({
   }
 }
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 export class regService extends RegistrationEmitters {
   protected listenOnSignUp = () => {
     console.log("inside here?");
-=======
-export class regService extends RegistrationEmitters {
-  protected listenOnSignUp = () => {
-<<<<<<< HEAD
->>>>>>> 392a674... event emitters added
-=======
-    console.log("inside here?");
->>>>>>> e70a359... resolve class dependency
+
     this.onDidSignUpEmitter.on(this.SIGNUP, async ({ email, password }) => {
       await signUpUser({ email, password });
     });
@@ -59,23 +40,4 @@ export class regService extends RegistrationEmitters {
       await signInUser({ email, password });
     });
   };
-<<<<<<< HEAD
-=======
-export class regService {
-  //   private context: Context | undefined;
-  //   constructor() {
-  //     this.initContext();
-  //   }
-  //   initContext() {
-  //     this.context = createContext<IformData[] | undefined>(undefined);
-  //   }
-  //   getContext() {
-  //     return this.context;
-  //   }
-  //   setContext(c: Context | undefined) {
-  //     this.context = c;
-  //   }
->>>>>>> 149a9bc... added the functionality to signup more abstractly
-=======
->>>>>>> 392a674... event emitters added
 }
--- movr-ui/src/components/Registration/RegistrationEmitters.ts
+++ movr-ui/src/components/Registration/RegistrationEmitters.ts
@@ -9,8 +9,6 @@ export default class RegistrationEmitters {
   protected SIGNUP = "SIGNUP";
   protected SIGNIN = "SIGNIN";
 
-<<<<<<< HEAD
-<<<<<<< HEAD
   protected readonly test = this.onDidSignInEmitter.eventNames();
 
   fireOnSignUp = (data: userData) => {
@@ -18,20 +16,6 @@ export default class RegistrationEmitters {
     console.log(test);
     this.onDidSignUpEmitter.emit(this.SIGNUP, data);
     console.log(test);
-=======
-=======
-  protected readonly test = this.onDidSignInEmitter.eventNames();
-
->>>>>>> e70a359... resolve class dependency
-  fireOnSignUp = (data: userData) => {
-    console.log(data, "printing data");
-    console.log(test);
-    this.onDidSignUpEmitter.emit(this.SIGNUP, data);
-<<<<<<< HEAD
->>>>>>> 392a674... event emitters added
-=======
-    console.log(test);
->>>>>>> e70a359... resolve class dependency
   };
 
   fireOnSignIn = (data: userData) => {
